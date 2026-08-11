bf6aace762411ad9f31763f2a991916751f21a2f
Karthik Raj <karthikraj9000@gmail.com>
2026-08-11 16:11:01 +0530

    chore: add new frontend URL to CORS allowed origins

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://127.0.0.1:5173',
   'http://localhost:4173',
+  'https://shilpa-frontend-nine.vercel.app',
   process.env.CLIENT_URL,
   process.env.FRONTEND_URL,
   process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : null,
